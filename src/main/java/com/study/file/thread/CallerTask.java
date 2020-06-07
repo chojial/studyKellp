2ce7364f5c4e427507bf2f2a785f37586e585f46
chojial <675325994@qq.com>
2020-06-07 20:39:54 +0800

    callable用法

--- src/main/java/com/study/file/thread/CallerTask.java
+++ src/main/java/com/study/file/thread/CallerTask.java
@@ -1,18 +1,19 @@
-//package com.study.file.thread;
-//
-//import java.util.concurrent.Callable;
-//
-///**
-// * @author：zhoujiale
-// */
-//public class CallerTask<String> implements Callable<String> {
-//    @Override
-//    public String call() throws Exception {
-//        return "hello";
-//    }
-//
-//    public static void main(String[] args) {
-//        CallerTask<String> callerTask = new CallerTask<>();
-//        new Thread(callerTask).start();
-//    }
-//}
+package com.study.file.thread;
+
+import java.util.concurrent.Callable;
+
+/**
+ * @author：zhoujiale
+ */
+public class CallerTask implements Callable<String> {
+    @Override
+    public String call() throws Exception {
+        return "hello";
+    }
+
+    public static void main(String[] args) throws Exception {
+        CallerTask callerTask = new CallerTask();
+        String call = callerTask.call();
+        System.out.println(call);
+    }
+}
